75f44b29b3e993fe0b6d42e5cfd14baccf115019
Wojtek Kostanski <wojtek@tappest.com>
2018-08-19 23:11:16 +0200

    add controllers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,6 +27,7 @@ require 'database_cleaner'
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
@@ -64,6 +65,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
     # add `FactoryBot` methods
     config.include FactoryBot::Syntax::Methods
+    config.include RequestSpecHelper, type: :request
 
     # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
     config.before(:suite) do
